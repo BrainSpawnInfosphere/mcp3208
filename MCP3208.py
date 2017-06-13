64d2fdeb2cacfb321282c7fbcea5fc3a79538df1
Kevin <walchko@users.noreply.github.com>
2017-06-13 14:12:01 -0600

    Create MCP3208.py

---
+++ MCP3208.py
@@ -0,0 +1,26 @@
+Adafruit_GPIO.SPI as SPI
+
+class MCP3208(object):
+	def __init__(self):
+		self.spi = SPI.SpiDev('/dev/spidev1.0', 0, 1000000)
+		self.spi.set_mode(0)
+		self.spi.set_bit_order(SPI.MSBFIRST)
+		
+	def __del__(self):
+		self.spi.close()
+	
+	def read(self, ch):
+		if 0 <= ch <= 7:
+			raise Exception('MCP3208 channel must be 0-7: ' + ch)
+			
+		cmd = 128  # 1000 0000
+		cmd += 64  # 1100 0000
+		cmd += ((ch & 0x07) << 3)
+		ret = self.spi.transfer([cmd, 0x0, 0x0])
+	
+		# get the 12b out of the return
+		val = (ret[0] & 0x01) << 11  # only B11 is here
+		val |= ret[1] << 3           # B10:B3
+		val |= ret[2] >> 5           # MSB has B2:B0 ... need to move down to LSB
+		
+		return (val & 0x0FFF)  # ensure we are only sending 12b
